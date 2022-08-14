da724961f1e75c555657464c6554561d83044fb9
Azmeer <m.azmeer2003@gmail.com>
2022-08-14 13:01:47 +0500

    Azmeer ka commit

--- function.py/function.py
+++ function.py/function.py
@@ -3,10 +3,11 @@ def main_fun(a, b):
     def addition(a, b):
         return a + b
 
-    # call inner function from outer function
+    # function ka locha
     add = addition(a, b)
     # add 5 to the result
     return add + 5
+
 
 result = main_fun(3, 5)
 print(result)
